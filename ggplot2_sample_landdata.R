083ef65d9d0f9746e06f8df322adcbe98005f478
Grejell Segura <mr_grej@yahoo.com>
2016-08-16 12:15:16 +0800

    Update ggplot2_sample_landdata.R

--- ggplot2_sample_landdata.R
+++ ggplot2_sample_landdata.R
@@ -33,9 +33,9 @@ any(is.na(data))
 g <- ggplot(data1, 
             aes(x = HV, 
                 y = SC, 
-            color = STATE)) + geom_point(size = 5, shape = 1)
+            color = STATE)) + geom_point(size = 9, shape = 1)
 
-g1 <- g + geom_point(size = 3)
+g1 <- g + geom_point(size = 7, alpha = .7)
 
 ##add a smoother
 g2 <- g1 + geom_smooth(method = 'lm', 
@@ -51,30 +51,31 @@ statelist
 #repel the labels
 
 g3 <- g2 + geom_text_repel(aes(label = STATE), 
-                           color = "gray20", 
+                           color = "gray10", 
                            data = subset(data1, STATE %in% statelist), 
                            force = 5, size = 4, family = 'Courier') #size is to change the font size of the label
 g3
-g4 <- g3 + scale_y_continuous(name = 'Structure Cost') + scale_x_continuous(name = 'Home Value')
+g4 <- g3 + scale_y_continuous(name = 'Structure Cost\n') + scale_x_continuous(name = '\nHome Value\n')
 
 #create different colors for every datapoints 
-colorvalue = c("#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC", "#248E84", "#F2583F", "#96503F",
-               "#24576D", "#099DD7", "#28AADC")
+colorvalue = c("#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235", "#A61407", "#5B0400", "#6fcb9f",
+               "#00515C", "#fb2e01", "#B38235")
 
-g5 <- g4 + scale_color_manual(values = colorvalue) + ggtitle('Relationship Between Home Value and Structure Cost in the US')
+g5 <- g4 + scale_color_manual(values = colorvalue) + ggtitle('Relationship Between Home Value and Structure Cost in the US\n')
 g6 <- g5 + geom_text(size = 2)
 
 
 
 g6 <- g5 +  theme_minimal() + theme(text = element_text(color = "gray20"),
                   plot.title = element_text(family = 'Courier', size = 15, face = "bold"),
+                  plot.margin = unit(c(.5,.5,.5,.5), "cm"),
                   legend.position = 'none',
                   axis.text = element_text(family = 'Courier'),
                   axis.title.x = element_text(family = 'Courier', vjust = -1, size = 15, face = 'bold'), # move title away from axis, size is the font size
@@ -85,4 +86,6 @@ g6 <- g5 +  theme_minimal() + theme(text = element_text(color = "gray20"),
                   panel.grid.major = element_line(color = "gray50", size = 0.5),
                   panel.grid.major.x = element_blank())
 g6
+
+
 
