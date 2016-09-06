068eeb4a897bfe10ce7fe9e47ad243b95b447159
Grejell Segura <mr_grej@yahoo.com>
2016-09-06 20:09:24 +0800

    Update digit_recognizer_SVM.R

--- digit_recognizer_SVM.R
+++ digit_recognizer_SVM.R
@@ -1,8 +1,8 @@
 ##EXERCISE USING SVM
 set.seed(123)
-library(e1071)
-library(caret)
-library(dplyr)
+library(e1071) # for svm
+library(caret) # for dummVars()
+
 
 #load the digits data - C:\Users\user\Desktop\Databank\Kaggle Competitions\Digit Recognizer
 digit <- read.csv(file.choose(), header = T, sep = ',')
@@ -21,11 +21,10 @@ View(labels)
 
 ##another way of creating dummy variables -- 
 ##just an example, will not be used in this exercise
-labels2 <- dummyVars(~labels, data = digit)
-labels3 <- predict(labels2, digit)
-View(labels3)
 
-?dummyVars
+# labels2 <- dummyVars(~labels, data = digit)
+# labels3 <- predict(labels2, digit)
+# View(labels3)
 
 #set an index for training and test data
 
@@ -35,11 +34,14 @@ index <- sample(t, round(nrow(digit)*.8))
 
 
 ###########################################################################
-#create an error vector to compare each error rates
-error <- NULL
+#TUNE THE MODEL# THIS MAY TAKE A LOOOOOOOOOOOOOOOOOT OF TIME
+#######################################################################
+##loop for tuning the model
 
-##loop the training process
 columns <- colnames(labels)
+bestgamma <- NULL
+bestcost <- NULL
+
 i = 1
 
 for (i in 1:10) {
@@ -56,62 +58,72 @@ for (i in 1:10) {
   f1 <- as.formula(paste(columns[i],'~.', sep = ''))
   
   #train the model using the train data
-  svmmodel <- svm(f1, data = train, scale = F)
+  tunedsv <- tune.svm(f1, data = train, gamma = 2^(-10:10), cost = 2^(-10:10), scale = FALSE)
   
-  #predict using the test data
+  #get the best gamma and cost
+  best_para <- tunedsv$best.parameters
+  
+  #store the best gamma and cost
+  bestgamma[i] <- best_para$gamma
+  bestcost[i] <- best_para$cost
+  
+    }
+
+best_parameters <- cbind(bestgamma, bestcost)
+best_parameters
+
+
+##############################################
+## TRAIN THE MODEL USING THE BEST PARAMETERS #
+##############################################
+
+#create an error vector to compare each error rates
+error <- NULL
+
+##loop the training process
+columns <- colnames(labels)
+best_parameters
+
+i = 1
+
+for (i in 1:10) {
+  
+  digitdata <- cbind(labels[,i], digit[,-1])
+  names(digitdata)[1] <- columns[i]
+  
+  #create train and test data
+  train <- digitdata[index,]
+  test <- digitdata[-index,]
+  
+  
+  #create a formula first
+  f1 <- as.formula(paste(columns[i],'~.', sep = ''))
+  
+  #train the model using the train data
+  svmmodel <- svm(f1, data = train, gamma = bestgamma[i], cost = bestcost[i], scale = FALSE)
+  
+  ##save the model as .rda##
+  #create a unique filename first
+  filename <- paste(columns[i],'model','.rda', sep = '')
+
+  save(svmmodel, file = filename)
+
+    #predict using the test data
   pred1 <- predict(svmmodel, test)
   
   #convert the values 
   pred2 <- ifelse(pred1 > 0.5 , 1, 0)
   
   #calculate the error
-  totalerror <- ifelse(test[,1] == pred2, 0, 1)
+  totalerror <- test[,1] - pred2
   
   #compute the model accuracy
-  err <-  sum(totalerror)/ nrow(test[,1])
+  err <-  sum(totalerror)/ sum(as.numeric(test[,1]))
   error[i] <- err
-  
 }
 
 ##show the error vector
 error
-
-#######################################################################
-##another loop for tuning the model
-
-i = 1
-bestgamma <- NULL
-bestcost <- NULL
-
-for (i in 1:10) {
-  
-  digitdata <- cbind(labels[,i], digit[,-1])
-  names(digitdata)[1] <- columns[i]
-  
-  #create train and test data
-  train <- digitdata[index,]
-  test <- digitdata[-index,]
-  
-  
-  #create a formula first
-  f1 <- as.formula(paste(columns[i],'~.', sep = ''))
-  
-  #train the model using the train data
-  tunedsv <- tune.svm(f1, data = train, gamma = 2^(-5:15), cost = 2^(-15:3))
-  
-  #get the best gamma and cost
-  gamma <- tunedsv$best.model@gamma
-  cost <- tunedsv$best.model@cost
-  
-  #store the best gamma and cost
-  bestgamma[i] <- gamma
-  bestcost[i] <- cost
-  }
-
-best_parameters <- cbind(bestgamma, bestcost)
-best_parameters
-bestgamma
-bestcost
 
 #####################################################################
 ##create a function for prediction
@@ -128,15 +140,16 @@ predictsvm <- function(x){
  label_target <- c(0:9)
  
  #loop the training
- i = 1
  for (i in 1:10) {
   
-  f2 <- as.formula(paste(columns[i],'~.', sep = ''))
+  filename <- paste(columns[i],'model','.rda', sep = '')
 
-  finalsvm <- svm(f2, data = train, gamma = bestgamma[i], cost = bestcost[i])
+  load(filename)
+
+
 
   #predict
-  predicted <- predict(finalsvm, x)
+  predicted <- predict(svmmodel, x)
   
   #gather the probability
   prob_gather[i] <- predicted
@@ -150,7 +163,7 @@ predictsvm <- function(x){
 #PERFORM A TEST
 
 test <- digit[-index,]
-x <- test[1,-1]
+x <- test[1,]
 
 predictsvm(x)
 test
